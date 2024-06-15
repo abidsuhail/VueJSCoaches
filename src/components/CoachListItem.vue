6d390fcea2fd3f4117b27bc9cbdd4bc99ae55032
Abid Suhail <asuhail@provensolution.com>
2024-06-15 14:22:18 +0530

    some changes

--- src/components/CoachListItem.vue
+++ src/components/CoachListItem.vue
@@ -1,17 +1,35 @@
 <template>
     <div class="parent-coach-item-div">
-        
+        <h2>{{coach.title}}</h2>
+        <p>{{ coach.time }}</p>
+        <button v-for="skill in coach.skills" :key="skill">{{ skill }}</button>
+        <div class="contact-details-container">
+            <button>Contact</button>
+            <button>View Details</button>
+        </div>
     </div>
 </template>
 
 <script>
-
+export default {
+    props:{
+        coach:{
+            type : Object,
+            required : true
+        }
+    }
+}
 </script>
 
 <style scoped>
 .parent-coach-item-div{
-    box-shadow: inset 0 0 10px #9b9b9b;
+    border: 2px solid black;
     border-radius: 10px;
     padding: 20px;
+    margin:15px;
+ }
+ .contact-details-container{
+    display: flex;
+    justify-content: end;
  }
 </style>
--- src/pages/AllCoaches.vue
+++ src/pages/AllCoaches.vue
@@ -1,16 +1,35 @@
 <template>
     <FindCoach></FindCoach>
+    <div id="coach-list-container">
+      <button>Refresh</button>
+      <CoachListItem v-for="coach in coaches" :key="coach.title" :coach="coach"></CoachListItem>
+    </div>
 </template>
 
 <script>
 import FindCoach from '../components/FindCoach.vue'
+import CoachListItem from '../components/CoachListItem.vue'
 export default {
     components:{
-        FindCoach
+        FindCoach,
+        CoachListItem
+    },
+    data(){
+        return{
+            coaches:[
+                {title:"Abid Suhail",time:"12:45",skills:["FrontEnd","BackEnd"]},
+                {title:"Arish Suhail",time:"1:45",skills:["FrontEnd"]}
+            ]
+        }
     }
 }
 </script>
 
 <style scoped>
-
+#coach-list-container{
+    box-shadow: inset 0 0 10px #9b9b9b;
+    border-radius: 10px;
+    padding: 20px;
+    margin-top: 10px;
+ }
 </style>
